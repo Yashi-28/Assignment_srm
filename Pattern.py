21a2346a59f833bc1b617e1ec81cefe0603b54c5
Yashi Srivastava <yashisrivastava800@gmail.com>
2025-08-18 01:03:18 +0530

    check

--- Pattern.py
+++ Pattern.py
@@ -1,4 +1,4 @@
 for i in range(1, 6):        # Rows from 1 to 5
     for j in range(1, i + 1):  # Numbers in each row
         print(j,end="")       # Print numbers withoutspace
-    print()  #Move to row in the nextline
+    print()  #Move to row in the next line
